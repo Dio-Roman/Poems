a82148db1b515c450e1fc5786b0fb2789befd02a
Dio-Roman <dio-roman@mail.ru>
2018-05-30 21:50:17 +0300

    фильтрация, стих подробно, фон у стиха

--- public/js/css.js
+++ public/js/css.js
@@ -1,21 +1,50 @@
 "use strict";
+// import images from "images.js";
+// import {IMAGES} from 'images.js';
 
-// показывает url
-// let imgSt = getComputedStyle(document.getElementById('img2'));
-// console.log(imgSt.backgroundImage);
+const IMAGES = ["1-nezn.jpeg", "2-chto.jpeg", "3-fontan.jpeg", "4-zima.jpeg", "5-lyric.jpeg", "6-zima.jpeg", "7-razgul.jpeg", "8-razmysh.jpeg"];
 
+// меняет url с нужной картинкой
 
-const IMAGES = ["1-nezn.jpeg", "2-chto.jpeg", "3-fontan.jpeg"];
+let mainCardImg = document.querySelectorAll('.main__card-img');
 
-// меняет url
-// let imgID = document.getElementById('img2');
-// imgID.style.backgroundImage = `url('../static/img/${IMAGES[0]}')`;
-
-
-let cardsArr = document.querySelectorAll('.main__card__img');
-console.log(cardsArr);
-console.log(cardsArr.length);
-
-for (let index = 0; index < cardsArr.length; index++) {
-  cardsArr[index].style.backgroundImage = `url('../static/img/${IMAGES[index]}')`
+for (let index = 0; index < mainCardImg.length; index++) {
+  mainCardImg[index].style.backgroundImage = `url('../static/img/${IMAGES[index]}')`
 }
+
+// проверка на наличие Названия стиха и добавляет класс с текст-в-fade
+
+let mainCard = document.querySelectorAll(".main__card");
+let mainCardNmame = document.querySelectorAll(".main__card-name")
+// let mainCardImg = document.querySelectorAll(".main__card__img");
+for (let j = 0; j < mainCard.length; j++) {
+  if(!mainCard[j].querySelector('.main__card-title').innerText) {
+    mainCardImg[j].style.backgroundImage = '';  //удалить фон картинку
+    mainCardImg[j].classList.add("text-fade");  //добав. класс с fade
+  }
+}
+
+// ----------фильтр-----
+
+let selectArr = document.querySelector(".select")
+let mainCardArr = document.querySelectorAll(".main__card");
+
+// фильтрует по жанрам сравнивая value и название класса
+function sort (value) {
+  for(let k = 0; k < mainCardArr.length; k++) {
+    mainCardArr[k].style.display = "initial";
+    if(value != "all") {
+      if(mainCardArr[k].classList[1]!= value) {
+            mainCardArr[k].style.display = "none";
+        }
+    }
+}
+}
+// показ. значение выделенного элемента 
+function selectIndex (){
+  let numberIndex = selectArr.options.selectedIndex;
+  let selectValue = selectArr.options[numberIndex].value;
+
+  sort(selectValue);
+}
+
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser= require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const dbPath = require('./db/dbPath');
 
 var db;
 
-MongoClient.connect('...', (err, client) => {
+MongoClient.connect(dbPath.mongo, (err, client) => {
     if (err) return console.log(err)
     db = client.db('poems') 
     app.listen(3000, () => {
@@ -30,6 +31,26 @@ MongoClient.connect('...', (err, client) => {
     
     // res.send(result)
     // console.log(result)
+  })
+})
+
+// ---
+// pp.get('/test/:name', function (req, res) {
+//   res.render('index', { title: 'Hey name', message: `Hello ${req.params["name"]}!` }
+// ---
+
+// выводит стих по id orderNumber
+var ObjectID = require('mongodb').ObjectID;
+
+app.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const details = { '_id': new ObjectID(id) };
+  db.collection('poems').find(details).toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('card.pug', {mongo: result})
+    // res.send(result)
+    // console.log(result)
+
   })
 })
 
