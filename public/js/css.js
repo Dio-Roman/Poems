70bccb8a1ab3a837133be56d0f8fee79d956b67c
Dio-Roman <dio-roman@mail.ru>
2018-07-10 19:06:03 +0300

    all good

--- public/js/css.js
+++ public/js/css.js
@@ -1,20 +1,20 @@
 "use strict";
 
-const IMAGES = ["1-nezn.jpeg", "2-chto.jpeg", "3-fontan.jpeg", "4-zima.jpeg", "5-lyric.jpeg", "6-zima.jpeg", "7-razgul.jpeg", "8-razmysh.jpeg", "9-zvezda.jpeg", "10-noch.jpeg", "11-jizn.jpeg", "12-instrum.jpeg", "13-milyi.jpeg", "14-galvan.jpeg", "15-shkola.jpeg", "16-zabot.jpeg", "17-plach.jpeg", "18-rybolov.jpeg", "19-odnu.jpeg", "20-skazka.jpeg"];
+let IMAGES = ["1-nezn.jpeg", "2-chto.jpeg", "3-fontan.jpeg", "4-zima.jpeg", "5-lyric.jpeg", "6-zima.jpeg", "7-razgul.jpeg", "8-razmysh.jpeg", "9-zvezda.jpeg", "10-noch.jpeg", "11-jizn.jpeg", "12-instrum.jpeg", "13-milyi.jpeg", "14-galvan.jpeg", "15-shkola.jpeg", "16-zabot.jpeg", "17-plach.jpeg", "18-rybolov.jpeg", "19-odnu.jpeg", "20-skazka.jpeg"];
 
-// меняет url с нужной картинкой
+
 
 let mainCardImg = document.querySelectorAll('.main__card-img');
+let mainCard = document.querySelectorAll(".main__card");
+let mainCardNmame = document.querySelectorAll(".main__card-name");
 
-for (let index = 0; index < mainCardImg.length; index++) {
-  mainCardImg[index].style.backgroundImage = `url('../static/img/${IMAGES[index]}')`
-}
+// меняет url с нужной картинкой
+// for (let index = 0; index < mainCardImg.length; index++) {
+//   mainCardImg[index].style.backgroundImage = `url('../static/img/${IMAGES[index]}')`
+// }
 
 // проверка на наличие Названия стиха и добавляет класс с текст-в-fade
 
-let mainCard = document.querySelectorAll(".main__card");
-let mainCardNmame = document.querySelectorAll(".main__card-name")
-// let mainCardImg = document.querySelectorAll(".main__card__img");
 for (let j = 0; j < mainCard.length; j++) {
   if (!mainCard[j].querySelector('.main__card-title').innerText) {
     mainCardImg[j].style.backgroundImage = ''; //удалить фон картинку
